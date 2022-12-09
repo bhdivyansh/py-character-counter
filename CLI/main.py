8309598424085b940231f2d4cb0b175fe21dcd2e
bhdivyansh <bh.divyansh.w@gmail.com>
2022-12-09 15:38:37 +0530

    Initial commit

---
+++ CLI/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Character counter CLI")
+print("You can paste a paragraph or a sentence below.")
+given_paragraph = input("Enter paragraph/sentence to count characters: \n\n")
+print("The length of the given sentence is", len(given_paragraph))
